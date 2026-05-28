5a4900fe501a4461e03c72848f367cfc78434543
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-05-28 23:19:11 +0530

    TREE

--- Tree/tree.py
+++ Tree/tree.py
@@ -1,6 +1,10 @@
 class Node:
-    def __inti__(self,data):
+    def __init__(self,data):
         self.data=data
         self.left=None
         self.right=None
          
+root=Node(5)
+root.left=Node(10)
+root.left.right=Node(2)
+print(root.left.right.data)
