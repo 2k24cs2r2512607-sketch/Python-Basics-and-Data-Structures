e740a0fe000b33ebd44517a39f010caf3ccf7abc
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-05-31 22:17:11 +0530

    Tree

---
+++ Tree/preorder.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,data):
+        self.ans=[]
+        self.data=data
+        self.left=None
+        self.right=None
+    def preorder(self,root):
+        if root is None:
+            return self.ans
+        self.ans(root.data)
+        self.preorder(root.left)
+        self.preorder(root.right)    
+root=Node(5)
+root.left=Node(10)
+root.left.right=Node(2)
+root.right=Node(13)
+ 
