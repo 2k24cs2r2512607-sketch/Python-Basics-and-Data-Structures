a9971a485559235556ca6cda839555917f94f234
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-06-02 18:33:09 +0530

    Queue

--- Queue/implement_by_list.py
+++ Queue/implement_by_list.py
@@ -1,28 +1,36 @@
 class Queue:
     def __init__(self):
-        self.q=[]
+        self.q = []
         self.front = -1
-    def push(self,x):
-        if self.front==-1:
-            self.front += 1      
+
+    def push(self, x):
+        if self.front == -1:
+            self.front = 0
         self.q.append(x)
-        return self.q
+
     def pop(self):
-        if self.front==-1:
-            return "Empty Queue"
-        
-        x=self.q[self.front]
+        if self.front == -1:
+            return None
+
+        x = self.q[self.front]
         self.front += 1
+
         if self.front == len(self.q):
             self.front = -1
             self.q = []
+
         return x
+
     def getfront(self):
-        if len(self.q)==0:
-            return "Empty Queue"
+        if self.front == -1:
+            return None
         return self.q[self.front]
+
     def size(self):
-        return len(self.q)-self.front
+        if self.front == -1:
+            return 0
+        return len(self.q) - self.front
+
 
 queue=Queue()
 queue.push(5)
