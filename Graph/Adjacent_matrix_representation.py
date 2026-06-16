2281ffc09d8a12ac8ed002b0c31e466bbc81bc9b
2k24cs2r2512607-sketch <2k24.cs2r2512607@gmail.com>
2026-06-16 18:34:07 +0530

    Graph

--- Graph/Adjacent_matrix_representation.py
+++ Graph/Adjacent_matrix_representation.py
@@ -3,13 +3,13 @@
 n=6#number of vertices
 e=7#number of edges
 edges=[(0,1), (0,3), (0,4), (1,2), (1,5), (2,4), (3,4)]  # edges
-adjList=[]
+adjMatrix=[]
 for i in range(n):
-    adjList.append([])
+    adjMatrix.append([-1]*n)
 for edge in edges:
     x=edge[0]
     y=edge[-1]
-    adjList[x].append(y)
-    adjList[y].append(x)
-for i in range(n):
-    print(i,"->",adjList[i])
+    adjMatrix[x][y]=1
+    adjMatrix[y][x]=1
+for i in adjMatrix:
+    print(i)
